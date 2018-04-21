309b9963a6c79cfb60e7512b8d80697a51df6b04
Javier Aguera <javibm6@gmail.com>
2018-04-21 14:25:56 +0200

    * Player Prebaf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  void Update() 
-	{
-    if (Input.GetMouseButton(0))
+    void Update()
     {
-      transform.position += Time.deltaTime * speed * transform.forward;
+        if (Input.GetMouseButton(0))
+        {
+            transform.position += Time.deltaTime * speed * transform.forward;
+        }
     }
-  }
- 
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 
-  void FixedUpdate()
-  {
-    Plane playerPlane = new Plane(Vector3.up, transform.position);
-
-    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-    float hitdist = 0.0f;
-    
-    if (playerPlane.Raycast(ray, out hitdist))
+    void FixedUpdate()
     {
-      Vector3 targetPoint = ray.GetPoint(hitdist);
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
 
-      Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-      transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        float hitdist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotation * Time.deltaTime);
+        }
     }
-  }
 
-  private Transform target;
+    private Transform target;
 
-  [SerializeField]
-  private float speed;
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float speedRotation;
 }
