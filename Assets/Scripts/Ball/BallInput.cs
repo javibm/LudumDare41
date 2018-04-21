d854897ea238cf024723d25232db97f29706bbbc
CarlotaEsca <carlota.esca91@gmail.com>
2018-04-21 15:48:32 +0200

    + Ball Movement improved

--- Assets/Scripts/Ball/BallInput.cs
+++ Assets/Scripts/Ball/BallInput.cs
@@ -20,8 +20,10 @@ public class BallInput : MonoBehaviour {
   bool chargingUpwards = true;
   float currentCharge;
 
-	// Use this for initialization
-	void Start () {
+  private Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+  // Use this for initialization
+  void Start () {
 	}
 	
 	// Update is called once per frame
@@ -29,29 +31,34 @@ public class BallInput : MonoBehaviour {
     if (mouseDown)
     {
       getDirection();
-
-      chargeShot();
-
-      Debug.LogError(currentCharge);
     }
 	}
 
   private void OnMouseDown() {
     currentCharge = 0;
     mouseDown = true;
+
     initialMousePosition = Input.mousePosition;
   }
 
   private void OnMouseUp() {
     mouseDown = false;
 
-    GetComponent<BallMovement>().MoveBall(-finalDirection * currentCharge);
+    GetComponent<BallMovement>().MoveBall(-finalDirection * 1000);
   }
 
   private void getDirection() {
-    finalDirection = Input.mousePosition - initialMousePosition;
-    finalDirection.z = finalDirection.y;
-    finalDirection.y = 0;
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    float enter;
+    if (plane.Raycast(ray, out enter))
+    {
+      var hitPoint = ray.GetPoint(enter);
+      finalDirection = hitPoint - transform.position;
+      finalDirection = Vector3.Normalize(finalDirection);
+      finalDirection.y = 0;
+    }
+
   }
 
   private void chargeShot()
