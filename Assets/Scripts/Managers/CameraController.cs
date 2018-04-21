c2abb77e4df18d62b5c3990e20934e55c6ccf9aa
Javier Aguera <javibm6@gmail.com>
2018-04-21 17:39:43 +0200

    * Added CameraController

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -8,7 +8,13 @@ public class CameraController : MonoBehaviour
         GameController.OnPlayerMovementTurn += ZoomIn;
         GameController.OnMinigolfTurn += ZoomOut;
 
+        camera = GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        camera.orthographicSize = normalZoom;
+
         offset = transform.position - player.transform.position;
+        zoomOffset = normalZoom - minZoom;
     }
 
     void Update()
@@ -18,16 +24,64 @@ public class CameraController : MonoBehaviour
 
     private void ZoomIn()
     {
-
+        StopAllCoroutines();
+        if (camera.orthographicSize != minZoom)
+        {
+            StartCoroutine(ZoomInCamera(zoomOffset));
+        }
     }
 
     private void ZoomOut()
     {
+        StopAllCoroutines();
+        if (camera.orthographicSize != normalZoom)
+        {
+            StartCoroutine(ZoomOutCamera(zoomOffset));
+        }
+    }
 
+    private IEnumerator ZoomInCamera(float zoom)
+    {
+        float time = 0.0f;
+        float cameraZoom = camera.orthographicSize;
+
+        while (time < timeTransition)
+        {
+            camera.orthographicSize = cameraZoom - zoom * zoomCurve.Evaluate(time / timeTransition);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        camera.orthographicSize = minZoom;
+    }
+
+    private IEnumerator ZoomOutCamera(float zoom)
+    {
+        float time = 0.0f;
+        float cameraZoom = camera.orthographicSize;
+
+        while (time < timeTransition)
+        {
+            camera.orthographicSize = cameraZoom + zoom * zoomCurve.Evaluate(time / timeTransition);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        camera.orthographicSize = normalZoom;
     }
 
     [SerializeField]
+    private AnimationCurve zoomCurve;
+    [SerializeField]
+    private float minZoom;
+    [SerializeField]
+    private float normalZoom;
+    [SerializeField]
+    private float timeTransition;
+
     private GameObject player;
 
     private Vector3 offset;
+
+    private Camera camera;
+
+    private float zoomOffset;
 }
