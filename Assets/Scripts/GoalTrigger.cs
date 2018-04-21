0da63410d63ffd90be7f1d0a5bd658b271d4177d
CarlotaEsca <carlota.esca91@gmail.com>
2018-04-21 17:49:39 +0200

    * Goal trigger implemented

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour {
+  [SerializeField]
+  private float velocityMagnitudeWin = 0.75f;
+
+  private Rigidbody ballRigidbody;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Ball") && ballRigidbody == null)
+    {
+      ballRigidbody = other.GetComponent<Rigidbody>();
+    }
+  }
+
+  private void OnTriggerStay(Collider other)
+  {
+    if (other.CompareTag("Ball"))
+    {
+      Debug.Log(ballRigidbody.velocity.magnitude);
+      if (ballRigidbody.velocity.magnitude < velocityMagnitudeWin)
+      {
+        Debug.LogError("TERMINÓ");
+        //GameController.Instance.EndGame();
+      }
+      else
+      {
+        Debug.Log("CASI!");
+      }
+    }
+  }
+}
