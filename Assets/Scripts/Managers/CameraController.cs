cb721a52ea7bfecadbe6d2d12c6d1f6f27e47b40
Javier Aguera <javibm6@gmail.com>
2018-04-22 13:49:59 +0200

    * Fixed ball player prefab

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -3,139 +3,139 @@ using System.Collections;
 
 public class CameraController : Singleton<CameraController>
 {
-  void Start()
-  {
-    GameController.OnPlayerMovementTurn += ZoomIn;
-    GameController.OnMinigolfTurn += ZoomOut;
-    GameController.OnResetBallPosition += ResetPosition;
+    void Start()
+    {
+        GameController.OnPlayerMovementTurn += ZoomIn;
+        GameController.OnMinigolfTurn += ZoomOut;
+        GameController.OnResetBallPosition += ResetPosition;
 
-    camera = GetComponent<Camera>();
-    LookForPlayerAndBall();
+        camera = GetComponent<Camera>();
+        LookForPlayerAndBall();
 
-    camera.orthographicSize = minZoom;
+        camera.orthographicSize = minZoom;
 
-    zoomOffset = normalZoom - minZoom;
-  }
+        zoomOffset = normalZoom - minZoom;
+    }
 
-  void OnDestroy()
-  {
-    GameController.OnPlayerMovementTurn -= ZoomIn;
-    GameController.OnMinigolfTurn -= ZoomOut;
-    GameController.OnResetBallPosition -= ResetPosition;
-  }
+    void OnDestroy()
+    {
+        GameController.OnPlayerMovementTurn -= ZoomIn;
+        GameController.OnMinigolfTurn -= ZoomOut;
+        GameController.OnResetBallPosition -= ResetPosition;
+    }
 
     private void LookForPlayerAndBall()
-  {
-    player = GameObject.FindGameObjectWithTag("Player");
-    ball = GameObject.FindGameObjectWithTag("Ball");
-    if(player == null || ball == null)
     {
-      MapGenerator.OnPlayerSpawned += LookForPlayerAndBall;
-      return;
+        player = GameObject.FindGameObjectWithTag("Player");
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        if (player == null || ball == null)
+        {
+            MapGenerator.OnPlayerSpawned += LookForPlayerAndBall;
+            return;
+        }
+        else
+        {
+            FollowCharacter();
+        }
     }
-    else
+
+    void Update()
     {
-      FollowCharacter();
+        if (cameraTarget != null)
+        {
+            finalPosition = cameraTarget.transform.position + new Vector3(10.0f, 10.0f, 10.0f);
+            transform.position = finalPosition;
+        }
     }
-  }
 
-  void Update()
-  {
-    if (cameraTarget != null)
+    void ResetPosition()
     {
-      finalPosition = cameraTarget.transform.position + new Vector3(6.0f, 6.0f, 6.0f);
-      transform.position = finalPosition;
+        FollowCharacter();
     }
-  }
 
-  void ResetPosition()
-  {
-    FollowCharacter();
-  }
-
-  public void FollowCharacter()
-  {
-    Time.timeScale = 1f;
-    cameraTarget = player;
-  }
-
-  public void FollowBall()
-  {
-    if (cameraTarget != ball)
+    public void FollowCharacter()
     {
-      cameraTarget = ball;
-      Time.timeScale = 0.5f;
+        Time.timeScale = 1f;
+        cameraTarget = player;
     }
-  }
 
-  private void ZoomIn()
-  {
-    StopAllCoroutines();
-    if (camera.orthographicSize != minZoom)
+    public void FollowBall()
     {
-      StartCoroutine(ZoomInCamera(zoomOffset));
+        if (cameraTarget != ball)
+        {
+            cameraTarget = ball;
+            Time.timeScale = 0.5f;
+        }
     }
-  }
 
-  private void ZoomOut()
-  {
-    StopAllCoroutines();
-    if (camera.orthographicSize != normalZoom)
+    private void ZoomIn()
     {
-      StartCoroutine(ZoomOutCamera(zoomOffset));
+        StopAllCoroutines();
+        if (camera.orthographicSize != minZoom)
+        {
+            StartCoroutine(ZoomInCamera(zoomOffset));
+        }
     }
-  }
 
-  private void StopZoom()
-  {
-    StopAllCoroutines();
-  }
-
-  private IEnumerator ZoomInCamera(float zoom)
-  {
-    float time = 0.0f;
-    float cameraZoom = camera.orthographicSize;
-
-    while (time < timeTransition)
+    private void ZoomOut()
     {
-      camera.orthographicSize = cameraZoom - zoom * zoomCurve.Evaluate(time / timeTransition);
-      time += Time.deltaTime;
-      yield return null;
+        StopAllCoroutines();
+        if (camera.orthographicSize != normalZoom)
+        {
+            StartCoroutine(ZoomOutCamera(zoomOffset));
+        }
     }
-    camera.orthographicSize = minZoom;
-  }
 
-  private IEnumerator ZoomOutCamera(float zoom)
-  {
-    float time = 0.0f;
-    float cameraZoom = camera.orthographicSize;
-
-    while (time < timeTransition)
+    private void StopZoom()
     {
-      camera.orthographicSize = cameraZoom + zoom * zoomCurve.Evaluate(time / timeTransition);
-      time += Time.deltaTime;
-      yield return null;
+        StopAllCoroutines();
     }
-    camera.orthographicSize = normalZoom;
-  }
 
-  [SerializeField]
-  private AnimationCurve zoomCurve;
-  [SerializeField]
-  private float minZoom;
-  [SerializeField]
-  private float normalZoom;
-  [SerializeField]
-  private float timeTransition;
+    private IEnumerator ZoomInCamera(float zoom)
+    {
+        float time = 0.0f;
+        float cameraZoom = camera.orthographicSize;
 
-  private GameObject player;
-  private GameObject ball;
-  private GameObject cameraTarget;
+        while (time < timeTransition)
+        {
+            camera.orthographicSize = cameraZoom - zoom * zoomCurve.Evaluate(time / timeTransition);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        camera.orthographicSize = minZoom;
+    }
 
-  private Vector3 finalPosition;
+    private IEnumerator ZoomOutCamera(float zoom)
+    {
+        float time = 0.0f;
+        float cameraZoom = camera.orthographicSize;
 
-  private new Camera camera;
+        while (time < timeTransition)
+        {
+            camera.orthographicSize = cameraZoom + zoom * zoomCurve.Evaluate(time / timeTransition);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        camera.orthographicSize = normalZoom;
+    }
 
-  private float zoomOffset;
+    [SerializeField]
+    private AnimationCurve zoomCurve;
+    [SerializeField]
+    private float minZoom;
+    [SerializeField]
+    private float normalZoom;
+    [SerializeField]
+    private float timeTransition;
+
+    private GameObject player;
+    private GameObject ball;
+    private GameObject cameraTarget;
+
+    private Vector3 finalPosition;
+
+    private new Camera camera;
+
+    private float zoomOffset;
 
 }
