800457d8f1473a9341b7108e87bf7cdb060d78d6
Javier Aguera <javibm6@gmail.com>
2018-04-22 19:32:40 +0200

    * OnPlayerWin event

--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -2,32 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoalTrigger : MonoBehaviour {
-  [SerializeField]
-  private float velocityMagnitudeWin = 0.75f;
+public class GoalTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private float velocityMagnitudeWin = 0.75f;
 
-  private Rigidbody ballRigidbody;
+    private Rigidbody ballRigidbody;
 
-  private void OnTriggerEnter(Collider other)
-  {
-    if (other.CompareTag("Ball") && ballRigidbody == null)
+    private void OnTriggerEnter(Collider other)
     {
-      ballRigidbody = other.GetComponent<Rigidbody>();
+        if (other.CompareTag("Ball") && ballRigidbody == null)
+        {
+            ballRigidbody = other.GetComponent<Rigidbody>();
+        }
     }
-  }
 
-  private void OnTriggerStay(Collider other)
-  {
-    if (other.CompareTag("Ball"))
+    private void OnTriggerStay(Collider other)
     {
-      if (ballRigidbody.velocity.magnitude < velocityMagnitudeWin)
-      {
-        ballRigidbody.useGravity = false;
-        ballRigidbody.transform.position = Vector3.Lerp(ballRigidbody.transform.position, _ballEndTransform.position, 5f * Time.deltaTime);
-      }
+        if (other.CompareTag("Ball"))
+        {
+            if (ballRigidbody.velocity.magnitude < velocityMagnitudeWin)
+            {
+                if (!_enterInHole)
+                {
+                    _enterInHole = true;
+                    GameController.Instance.PlayerWin();
+                }
+                ballRigidbody.useGravity = false;
+                ballRigidbody.transform.position = Vector3.Lerp(ballRigidbody.transform.position, _ballEndTransform.position, 5f * Time.deltaTime);
+            }
+        }
     }
-  }
 
-  [SerializeField]
-  private Transform _ballEndTransform;
+    [SerializeField]
+    private Transform _ballEndTransform;
+
+    private bool _enterInHole = false;
 }
