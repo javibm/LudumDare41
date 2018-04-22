0bb7ca9db1874c5df157b7073a2e5f708288de6c
CarlotaEsca <carlota.esca91@gmail.com>
2018-04-22 18:58:58 +0200

    * Fall dead implemented

---
+++ Assets/Scripts/LavaCollider.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaCollider : MonoBehaviour {
+
+  bool stopCollision = false;
+
+  private void OnEnable()
+  {
+    GameController.OnPlayerDead += StopCollision;
+  }
+
+  private void OnDestroy()
+  {
+    GameController.OnPlayerDead -= StopCollision;
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (!stopCollision && collision.gameObject.CompareTag("Player"))
+    {
+      GameController.Instance.PlayerDead();
+      StartCoroutine(AnimateCollider());
+    }
+  }
+
+  private void StopCollision()
+  {
+    stopCollision = true;
+  }
+
+  private IEnumerator AnimateCollider()
+  {
+    yield return new WaitForSeconds(0.5f);
+
+    float totalTime = 2;
+    float currentTime = 0;
+    Vector3 initialPosition = transform.position;
+
+    while (currentTime < totalTime)
+    {
+      transform.position = Vector3.Lerp(initialPosition, initialPosition + Vector3.down, currentTime / totalTime);
+      currentTime += Time.deltaTime;
+      yield return 0;
+    }
+    GameController.Instance.EndGame();
+  }
+}
